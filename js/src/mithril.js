760237cf082856f4c5690c3787cb5671c160e991
Boris Kaul <localvoid@gmail.com>
2014-10-26 12:22:55 +0600

    Simplify mithril code

--- js/src/mithril.js
+++ js/src/mithril.js
@@ -1,9 +1,5 @@
 var m = require('mithril');
 
-var emptyModule = {};
-emptyModule.controller = function() {};
-emptyModule.view = function() {};
-
 function mithrilBuildTree(nodes) {
   var children = [];
   for (var i = 0; i < nodes.length; i++) {
@@ -17,15 +13,6 @@ function mithrilBuildTree(nodes) {
   return children;
 }
 
-function MithrilModule(nodes) {
-  this.nodes = nodes;
-}
-
-MithrilModule.prototype.controller = function() {};
-MithrilModule.prototype.view = function() {
-  return [m('div', {}, mithrilBuildTree(this.nodes))];
-};
-
 function Benchmark(a, b, container) {
   this._a = a;
   this._b = b;
@@ -38,19 +25,15 @@ Benchmark.prototype.setUp = function() {
 };
 
 Benchmark.prototype.tearDown = function() {
-  m.module(this._container, emptyModule);
-  m.redraw(true);
+  m.render(this._container, '', true);
 };
 
 Benchmark.prototype.render = function() {
-  this._module = new MithrilModule(this._a);
-  m.module(this._container, this._module);
-  m.redraw(true);
+  m.render(this._container, m('div', {key: 0}, mithrilBuildTree(this._a)));
 };
 
 Benchmark.prototype.update = function() {
-  this._module.nodes = this._b;
-  m.redraw(true);
+  m.render(this._container, m('div', {key: 0}, mithrilBuildTree(this._b)));
 };
 
 module.exports = Benchmark;
