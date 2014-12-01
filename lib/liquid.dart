4afcc1dace59dd70f70a0fb7e2960d821b6e92d3
Boris Kaul <localvoid@gmail.com>
2014-12-01 08:40:47 +0600

    Update Liquid/VDom

--- lib/liquid.dart
+++ lib/liquid.dart
@@ -6,93 +6,98 @@ import 'package:vdom_benchmark/generator.dart' as g;
 import 'package:vdom/vdom.dart' as v;
 import 'package:liquid/liquid.dart';
 
-class NodeComponent extends VComponent {
-  g.Node _node;
+class NodeComponent extends Component<html.DivElement> {
+  g.Node node;
 
-  NodeComponent(ComponentBase parent, this._node)
-      : super('div', parent);
+  NodeComponent(Context context, this.node) : super(context);
 
-  v.Element build() {
+  VRoot build() {
     final result = [];
-    final children = _node.children;
+    final children = node.children;
     if (children != null) {
       for (var i = 0; i < children.length; i++) {
         final c = children[i];
         if (c.children == null) {
-          result.add(LeafComponent.virtual(c.key, c));
+          result.add(new VLeaf(c.key, c));
         } else {
-          result.add(NodeComponent.virtual(c.key, c));
+          result.add(new VNode(c.key, c));
         }
       }
     }
-    return new v.Element(0, 'div', result);
+    return new VRoot()(result);
   }
 
-  void updateProperties(g.Node node) {
-    _node = node;
-    if (_node.dirty) {
-      isDirty = true;
-      update();
-    }
-  }
-
-  static VDomComponent virtual(Object key, g.Node node) {
-    return new VDomComponent(key, (component, context) {
-      if (component == null) {
-        return new NodeComponent(context, node);
-      }
-      component.updateProperties(node);
-    });
-  }
-
-  String toString() => 'Node: [$_node]';
+  String toString() => 'Node: [$node]';
 }
 
-class LeafComponent extends VComponent {
-  g.Node _node;
+class LeafComponent extends Component<html.SpanElement> {
+  g.Node node;
 
-  LeafComponent(ComponentBase parent, this._node)
-      : super('span', parent);
+  LeafComponent(Context context, this.node) : super(context);
 
-  void updateProperties(g.Node node) {
-    _node = node;
-    if (_node.dirty) {
-      isDirty = true;
-      update();
-    }
+  void create() {
+    element = new html.SpanElement();
   }
 
-  static VDomComponent virtual(Object key, g.Node node) {
-    return new VDomComponent(key, (component, context) {
-      if (component == null) {
-        return new LeafComponent(context, node);
-      }
-      component.updateProperties(node);
-    });
+  VRoot build() {
+    return new VRoot()([new v.Text(0, node.key.toString())]);
   }
 
-  v.Element build() {
-    return new v.Element(0, 'span',
-        [new v.Text(0, _node.key.toString())]);
-  }
-
-  String toString() => 'Leaf: [${_node.key}]';
+  String toString() => 'Leaf: [${node.key}]';
 }
 
-class App extends VComponent {
+class VNode extends VComponentBase {
+  g.Node node;
+
+  VNode(Object key, this.node) : super(key);
+
+  void create(Context context) {
+    component = new NodeComponent(context, node);
+    component.create();
+    ref = component.element;
+  }
+
+  void update(VNode other, Context context) {
+    super.update(other, context);
+    component.node = other.node;
+    if (other.node.dirty) {
+      component.update();
+    }
+  }
+}
+
+class VLeaf extends VComponentBase {
+  g.Node node;
+
+  VLeaf(Object key, this.node) : super(key);
+
+  void create(Context context) {
+    component = new LeafComponent(context, node);
+    component.create();
+    ref = component.element;
+  }
+
+  void update(VLeaf other, Context context) {
+    super.update(other, context);
+    component.node = other.node;
+    if (other.node.dirty) {
+      component.update();
+    }
+  }
+}
+
+class App extends Component {
   List<g.Node> _nodes;
 
   set node(List<g.Node> newNodes) {
     _nodes = newNodes;
-    isDirty = true;
     update();
   }
 
-  App(ComponentBase parent, this._nodes) : super('div', parent);
+  App(Context context, this._nodes) : super(context);
 
-  v.Element build() {
-    return new v.Element(0, 'div',
-        [NodeComponent.virtual(0, new g.Node(0, true, _nodes))]);
+  VRoot build() {
+    return new VRoot()([new VNode(0, new g.Node(0, true, _nodes))]);
   }
 }
 
@@ -102,16 +107,15 @@ class Benchmark extends BenchmarkBase {
   List<g.Node> b;
   html.Element _container;
 
-  RootComponent _root;
-
   App _app;
-  html.Element _element;
 
   Benchmark(this.a, this.b, this._container) : super('VComponent');
 
   void render() {
-    _app = new App(_root, a);
-    _root.append(_app);
+    _app = new App(null, a);
+    _app.create();
+    _app.render();
+    _container.append(_app.element);
   }
 
   void update() {
@@ -119,10 +123,9 @@ class Benchmark extends BenchmarkBase {
   }
 
   void setup() {
-    _root = new RootComponent.mount(_container);
   }
 
   void teardown() {
-    _container.children.clear();
+    _app.element.remove();
   }
 }
--- lib/vdom.dart
+++ lib/vdom.dart
@@ -10,9 +10,9 @@ List<v.Node> vdomBuildTree(List<g.Node> nodes) {
   for (var i = 0; i < nodes.length; i++) {
     final n = nodes[i];
     if (n.children != null) {
-      children.add(new v.Element(n.key, 'div', vdomBuildTree(n.children)));
+      children.add(new v.Element(n.key, 'div')(vdomBuildTree(n.children)));
     } else {
-      children.add(new v.Element(n.key, 'span', [new v.Text(0, n.key.toString())]));
+      children.add(new v.Element(n.key, 'span')([new v.Text(0, n.key.toString())]));
     }
   }
   return children;
@@ -28,13 +28,13 @@ class Benchmark extends BenchmarkBase {
   Benchmark(this.a, this.b, this._container) : super('VDom');
 
   void render() {
-    _vRoot = new v.Element(0, 'div', vdomBuildTree(a));
-    _vRoot.inject(_container, const v.Context(false));
+    _vRoot = new v.Element(0, 'div')(vdomBuildTree(a));
+    v.inject(_vRoot, _container, const v.Context(false));
   }
 
   void update() {
-    final newVroot = new v.Element(0, 'div', vdomBuildTree(b));
-    _vRoot.sync(newVroot, const v.Context(false));
+    final newVroot = new v.Element(0, 'div')(vdomBuildTree(b));
+    _vRoot.update(newVroot, const v.Context(false));
   }
 
   void setup() {
