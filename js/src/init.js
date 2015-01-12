26ea78b53d30dba66383e4a6ca2752734db20c73
Boris Kaul <localvoid@gmail.com>
2015-01-12 09:06:09 +0600

    Add performance.now() polyfill #13

--- js/src/init.js
+++ js/src/init.js
@@ -1,3 +1,24 @@
+// performance.now() polyfill
+// https://gist.github.com/paulirish/5438650
+// prepare base perf object
+if (typeof window.performance === 'undefined') {
+  window.performance = {};
+}
+
+if (!window.performance.now){
+
+  var nowOffset = Date.now();
+
+  if (performance.timing && performance.timing.navigationStart){
+    nowOffset = performance.timing.navigationStart
+  }
+
+  window.performance.now = function now(){
+    return Date.now() - nowOffset;
+  };
+}
+
+
 function Node(key, dirty, children) {
   this.key = key;
   this.dirty = dirty;
