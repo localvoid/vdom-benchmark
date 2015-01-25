9ffa145449adb8fb194329b0e439b1ed0f135e79
Boris Kaul <localvoid@gmail.com>
2015-01-25 14:52:01 +0600

    Update

--- config.js
+++ config.js
@@ -8,26 +8,32 @@ benchmarkConfig({
   "contestants": [
     {
       "name": "kivi",
+      "url": "https://github.com/localvoid/kivi",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-kivi/"
     },
     {
       "name": "cito.js",
+      "url": "https://github.com/joelrich/citojs",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-cito/"
     },
     {
       "name": "Bobril",
+      "url": "https://github.com/Bobris/Bobril",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-bobril/"
     },
     {
       "name": "React",
+      "url": "http://facebook.github.io/react/",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-react/"
     },
     {
       "name": "virtual-dom",
+      "url": "https://github.com/Matt-Esch/virtual-dom",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-virtual-dom/"
     },
     {
       "name": "mithril",
+      "url": "http://lhorie.github.io/mithril/",
       "benchmarkUrl": "http://localvoid.github.io/vdom-benchmark-mithril/"
     }
   ]
