5dad7249b2ac5b7c25a380c1c711bb3bc53ab367
Boris Kaul <localvoid@gmail.com>
2015-01-25 14:17:21 +0600

    Update

---
+++ config.js
@@ -0,0 +1,22 @@
+benchmarkConfig({
+  "name": "VDom Benchmark",
+  "description": "Comparing performance of the diff/patch operations in various virtual dom libraries.",
+  "data": {
+    "type": "script",
+    "url": "https://localvoid.github.com/vdom-benchmark/generator.js"
+  },
+  "contestants": [
+    {
+      "name": "kivi",
+      "benchmarkUrl": "https://localvoid.github.com/vdom-benchmark-kivi/"
+    },
+    {
+      "name": "cito.js",
+      "benchmarkUrl": "https://localvoid.github.com/vdom-benchmark-cito/"
+    },
+    {
+      "name": "Bobril",
+      "benchmarkUrl": "https://localvoid.github.com/vdom-benchmark-bobril/"
+    }
+  ]
+});
